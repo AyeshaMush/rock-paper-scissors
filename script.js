6fac1166b07cc4060e89c5cc864d6647b8347801
AyeshaMush <140245130+AyeshaMush@users.noreply.github.com>
2024-11-23 05:19:49 -0800

    Add game replaying logic

--- script.js
+++ script.js
@@ -73,60 +73,72 @@ function getHumanChoice() {
 //function to play a single round of rock, paper and scissors
 function playRound(humanChoice, computerChoice) {
 
+    const outputText = document.getElementById('output-text');
     //human winning choices
     if (humanChoice === "rock" && computerChoice === "scissor") {
         humanScore++;
-        alert(`You win! Rock beats scissor! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You win! Rock beats scissor! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
     else if (humanChoice === "paper" && computerChoice === "rock") {
         humanScore++;
-        alert(`You win! Paper beats rock! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You win! Paper beats rock! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
     else if (humanChoice === "scissor" && computerChoice === "paper") {
         humanScore++;
-        alert(`You win! Scissor beats paper! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You win! Scissor beats paper! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
 
     //computer winning choices
     else if (computerChoice === "rock" && humanChoice === "scissor") {
         computerScore++;
-        alert(`You lose! Rock beats scissor! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You lose! Rock beats scissor! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
     else if (computerChoice === "paper" && humanChoice === "rock") {
         computerScore++;
-        alert(`You lose! Paper beats rock! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You lose! Paper beats rock! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
     else if (computerChoice === "scissor" && humanChoice === "paper") {
         computerScore++;
-        alert(`You lose! Scissor beats paper! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `You lose! Scissor beats paper! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
 
     //tie between computer and human
     else if (computerChoice === "rock" && humanChoice === "rock" || computerChoice === "paper" && humanChoice === "paper" || computerChoice === "scissor" && humanChoice === "scissor") {
-        alert(`It's a tie! Your score: ${humanScore} and computer score: ${computerScore}`);
+        outputText.innerText = `It's a tie! Your score: ${humanScore} and computer score: ${computerScore}`;
     }
 }
 
 //function to determine final winners
 function finalWinner() {
+    const outputText = document.getElementById('output-text');
     if (humanScore > computerScore) {
-        alert("Congratulations! You have won the game!");
+        outputText.innerText = "Congratulations! You have won the game!";
     }
     else if (humanScore < computerScore) {
-        alert("You have lost the game! Try again!");
+        outputText.innerText = "You have lost the game! Try again!";
     }
     else if (humanScore === computerScore) {
-        alert("It is a tie!");
+        outputText.innerText = "It is a tie!";
     }
+
+    const playAgainButton=document.getElementById("play-again-button");
+    playAgainButton.style.display = 'block';
+
+
+    playAgainButton.addEventListener('click', () => {
+        resetGame();
+        playAgainButton.style.display = 'none';
+    });
 }
 
-//function to play five rounds of rock paper scissors
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    const outputText = document.getElementById('output-text');
+    outputText.innerText = `Game reset. Your score: 0 and computer score: 0`;
+}
+
 function playGame() {
-
-    //welcome message and game rules
-    alert("Welcome to this game of rock, paper and scissors!");
-    alert("You will be playing against the computer. Remember that rock beats scissors, scissors beat paper, and paper beats rocks.");
-
 
     getHumanChoice();
 
